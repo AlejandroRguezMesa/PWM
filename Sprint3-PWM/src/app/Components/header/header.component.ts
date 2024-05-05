fdcc8303d342eb3a5446aab22f4afa108e125217
Adonai Hernandez Bolaños <adonai.acsii@gmail.com>
2024-05-05 13:02:18 +0100

    Login funcionando + o -

--- Sprint3-PWM/src/app/Components/header/header.component.ts
+++ Sprint3-PWM/src/app/Components/header/header.component.ts
@@ -1,6 +1,8 @@
-import {Component, inject} from '@angular/core';
+import {Component, HostListener, inject, input, Output} from '@angular/core';
 import {AuthService} from "../../auth.service";
 import {NgIf} from "@angular/common";
+import {loadEsmModuleFromMemory} from "@angular-devkit/build-angular/src/utils/server-rendering/load-esm-from-memory";
+import EventEmitter from "node:events";
 
 @Component({
   selector: 'app-header',
@@ -13,8 +15,28 @@ import {NgIf} from "@angular/common";
 })
 export class HeaderComponent {
   authService = inject(AuthService)
+  isLoginShowed: boolean = false;
+  constructor() {
 
+  }
   logout(): void {
     this.authService.logout();
+  }
+
+  toggleLogin(): void{
+    let login = document.querySelector(".flotante.login");
+    if (this.isLoginShowed){
+      login?.classList.remove("showed");
+      login?.classList.add("oculto");
+      this.isLoginShowed = false;
+    } else {
+      login?.classList.remove("oculto");
+      login?.classList.add("showed");
+      this.isLoginShowed = true;
+    }
+
+  }
+  public LoginOK(){
+    this.toggleLogin()
   }
 }
--- Sprint3-PWM/src/app/login/login.component.ts
+++ Sprint3-PWM/src/app/login/login.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, inject, Output} from '@angular/core';
+import {FormBuilder, FormsModule, Validators} from "@angular/forms";
+import {AuthService} from "../auth.service";
+import { Router } from '@angular/router';
+import { HeaderComponent } from "../Components/header/header.component";
+import EventEmitter from "node:events";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  fb = inject(FormBuilder);
+  authService = inject(AuthService)
+  email: string = "";
+  password: string = "";
+
+  form = this.fb.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  constructor(private router: Router) {
+  }
+  onSubmit(): void{
+    this.authService.login(this.email, this.password).subscribe({
+      next:()=>{
+        let returnUrl = localStorage.getItem('returnUrl');
+        this.router.navigateByUrl(returnUrl ? returnUrl : '/');
+        localStorage.removeItem('returnUrl');
+
+        let login = document.querySelector(".flotante.login");
+
+        login?.classList.remove("showed");
+        login?.classList.add("oculto");
+
+      },
+      error: (err) => {
+        console.log(err.code);
+      }
+    })
+  }
 
 }
